8720f36026c1d336f4c8d0c3e0ed63dc9959b206
Cikenote <cikenote@Cikenotes-MacBook-Pro.local>
2025-08-17 05:27:45 +0700

    delete notification deploy

--- next.config.mjs
+++ next.config.mjs
@@ -67,7 +67,7 @@ const nextConfig = {
         hostname: 'tse4.mm.bing.net',
         protocol: 'https',
         pathname: '/**',
-      }
+      },
     ],
     // Optimize image loading and caching
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
